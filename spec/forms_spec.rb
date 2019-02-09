2598df098cfa2a3f8c85019b254cf0494fbc6428
Michael Martins <michaelfm21@gmail.com>
2019-02-09 20:33:27 -0200

    formulários

---
+++ spec/forms_spec.rb
@@ -0,0 +1,24 @@
+
+describe 'Forms' do
+
+    it 'login com sucesso' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'username', with: 'stark'
+        fill_in 'password', with: 'jarvis!'
+
+        click_button 'Login'
+
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    end
+
+    it 'senha incorreta' do
+        
+    end
+
+    it 'usuário não cadastrado' do
+
+    end
+
+end
